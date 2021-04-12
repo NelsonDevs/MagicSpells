c584e7a42fca676d134dcec992b3a910f332355f
nelso <nelso@DESKTOP-Q6OP10J.hsd1.nj.comcast.net>
2021-04-12 17:55:52 -0400

    Wow this crazy

--- src/com/nisovin/magicspells/spells/ArrowSpell.java
+++ src/com/nisovin/magicspells/spells/ArrowSpell.java
@@ -32,7 +32,7 @@ import com.nisovin.magicspells.util.SpellReagents;
 public class ArrowSpell extends Spell {
 
 	private static ArrowSpellHandler handler;
-	// YeetMas
+	// YeetMas Figuring this :)
 	String bowName;
 	String spellNameOnHitEntity;
 	String spellNameOnHitGround;
